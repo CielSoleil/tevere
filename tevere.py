268ef9617dc8394e2e5742a9633c7fa841056647
C13L99 <6krBy96tu0@gmail.com>
2026-03-17 10:45:31 -0600

    [tevere] Create -json
    Passing this argument saves the json API to disk

--- tevere.py
+++ tevere.py
@@ -8,6 +8,7 @@ from loguru import logger as log
 def main():
     parser = ArgumentParser()
     parser.add_argument("url")
+    parser.add_argument("-json", action="store_true")
     parser.add_argument("-debug", action="store_true")
     args = parser.parse_args()
 
