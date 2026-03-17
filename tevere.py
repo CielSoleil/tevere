1f2e33e25a52cd104fa5dd149d1ccac987b13204
C13L99 <6krBy96tu0@gmail.com>
2026-03-17 10:50:27 -0600

    [tevere] Create get?id()
    This function accepts a tver.jp episode url and returns the episode id

--- tevere.py
+++ tevere.py
@@ -5,6 +5,20 @@ from argparse import ArgumentParser
 import niquests
 from loguru import logger as log
 
+def get_id(url: str) -> str:
+    """
+    Take a tver.jp url and extract the episode id
+    Return the id only
+    """
+    split_url = url.rsplit("/", 1)
+    log.debug(split_url)
+
+    episode_id = split_url[-1]
+    log.info("Id found: {}", episode_id)
+
+    return episode_id
+
+
 def main():
     parser = ArgumentParser()
     parser.add_argument("url")
