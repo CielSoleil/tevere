43dc9a22f6c433ea9128ae8cd2e032f2c096a86c
C13L99 <6krBy96tu0@gmail.com>
2026-03-17 10:39:00 -0600

    [tevere] Create custom INFO logger
    It is only triggered if -debug is not passed, as it contains less information

--- tevere.py
+++ tevere.py
@@ -10,6 +10,11 @@ def main():
     parser.add_argument("-debug", action="store_true")
     args = parser.parse_args()
 
+    if not args.debug:
+        fmt = "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
+        log.remove()
+        log.add(sys.stderr, level="INFO", format=fmt)
+
     return 0
 
 
