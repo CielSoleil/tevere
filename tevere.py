db3c479c0d2f4f0aca1edc53957070c9753986d1
C13L99 <6krBy96tu0@gmail.com>
2026-03-17 10:42:37 -0600

    [tevere] Add url argument

--- tevere.py
+++ tevere.py
@@ -7,6 +7,7 @@ from loguru import logger as log
 
 def main():
     parser = ArgumentParser()
+    parser.add_argument("url")
     parser.add_argument("-debug", action="store_true")
     args = parser.parse_args()
 
