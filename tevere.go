01b3627101952fe3c3a08208a5079b29d7bec0fb
C13L99 <6krBy96tu0@gmail.com>
2026-04-22 20:44:02 -0600

    Rewrite download function
    http.Get doesn't allow to set headers. Also Gemini

--- tevere.go
+++ tevere.go
@@ -7,8 +7,18 @@ import (
 )
 
 func download(url string, destination string) error {
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", url, nil)
+
+	// Custom headers
+	req.Header.Set("user-agent", "Mozilla/5.0")
+	req.Header.Set("accept", "*/*")
+	req.Header.Set("origin", "https://tver.jp")
+	req.Header.Set("referer", "https://tver.jp")
+	req.Header.Set("x-tver-platform-type", "web")
+
 	// Get the data
-	resp, err := http.Get(url)
+	resp, err := client.Do(req)
 	if err != nil {
 		return err
 	}
